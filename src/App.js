8f789651f64bb131553dd27bef64179425f14bb5
steffen <steffen@vittech.dk>
2021-04-26 14:25:06 +0200

    webapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from "axios"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+class App extends Component {
+
+
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+            items: []
+        };
+
+    }
+    componentDidMount() {
+        axios.get("http://localhost:5000/api/item/all").then(response => {
+            console.log(response)
+            this.setState = {
+                item: response.data
+            }
+        }).catch(error => {
+            console.log(error.message)
+        })
+    }
+    render() {
+        return ( <div>
+            <h1> List of Items </h1>
+</div>);
+        }
+
+    }
+
+    export default App;
